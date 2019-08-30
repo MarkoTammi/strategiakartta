fccdbf3fe0487e5c0e8dd3f582eab238e13dca77
Tammivuori <marko.tammivuori@gmail.com>
2019-08-30 10:56:34 +0300

    Text content separated to Txt.js

--- strategiakartta/src/App.js
+++ strategiakartta/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+// MarkoT Strategiakartta App.js
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import text from './Txt';
+
 
 function App() {
+
+    // Set language
+    const lang = "fi";
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <HeroElement text={text} lang={lang}/>
+
+    <Footer text={text} lang={lang}/> 
+
     </div>
-  );
+
+     
+  )
 }
 
+// Component to display the hero / jumbotron element
+const HeroElement = (props) =>{
+  console.log("HeroElement - props :". props)
+  return (
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-5 text-center">{props.text[props.lang][1001]}</h1>
+          <p className="lead text-center">{props.text[props.lang][1002]}</p>
+        </div>
+      </div>
+  )
+}
+
+// Component to display the footer
+const Footer = (props) => {
+  const footerStyle = {
+      color: 'green',
+      fontStyle: 'italic',
+      fontSize: '0.8em'
+    }
+
+  return (
+      <h6 style={footerStyle}>{props.text[props.lang][1003]}</h6>
+  )
+}
 export default App;
